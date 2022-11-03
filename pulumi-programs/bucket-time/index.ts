1dca12f2d3c344493fccd46d2adfb3d714d42aec
1ansel <ansel.lampe@gmail.com>
2022-11-03 15:49:04 -0700

    testing

--- pulumi-programs/bucket-time/index.ts
+++ pulumi-programs/bucket-time/index.ts
@@ -1,7 +1,7 @@
 import * as aws from "@pulumi/aws";
 
 // Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.Bucket("fork-bucket");
+const bucket = new aws.s3.Bucket("another-bucket");
 
 // Export the name of the bucket
 export const bucketName = bucket.id;
